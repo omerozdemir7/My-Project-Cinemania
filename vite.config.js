746bd9f6bc7ab948243c8f47bfa4c27c7c9903a9
Ömer ÖZDEMİR <ozd.omer17@gmail.com>
2025-11-20 19:40:36 +0300

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,15 @@ import injectHTML from "vite-plugin-html-inject";
 
 export default defineConfig(({ command }) => {
   return {
-    define: {
-      [command === "serve" ? "global" : "_global"]: {},
-    },
-    root: "src", // Kök dizin 'src' olduğu için
+    // ... diğer ayarların ...
+    
+    // BURAYI EKLE: GitHub Repo adını buraya yaz
+    base: "/my-project-cinemania/", 
+
+    root: "src", 
     build: {
-      outDir: "../dist", // DÜZELTME 1: Burası build'in hemen altında olmalı, rollupOptions içinde değil.
-      emptyOutDir: true, // DÜZELTME 2: Kök dizin dışını temizlemek için bu izin gereklidir.
+      outDir: "../dist", 
+      emptyOutDir: true,
       sourcemap: true,
       rollupOptions: {
         input: glob.sync("./src/*.html"),
